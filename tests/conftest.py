796ebf98f689b8aba9723c3eafb1f7623177beca
Klim Trotsenko <klim.trotsenko7@gmail.com>
2023-10-06 17:17:37 +0300

    update conftest.py and README.md

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,36 @@
 import pytest
-from selene import browser
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
 
-@pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    browser.config.base_url = 'https://staging.azalianow.shop/'
-    browser.config.driver_name = 'chrome'
-    browser.config.window_width = 1200
-    browser.config.window_height = 1200
-    yield
+from utils import attach
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    browser_version = "100.0"
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
+
